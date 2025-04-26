2f0fa958b21a630c8a6ceb8a64f77b2a1aec69f4
KirillMoz <kirill.mozzhegorov@yandex.ru>
2025-04-26 22:20:23 +0500

    Промежуточное закрытие

--- EFTest/Program.cs
+++ EFTest/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace EFTest
 {
-    internal class Program
+    class Programm
     {
         static void Main(string[] args)
         {
+            using (var db = new AppContext())
+            {
+                var user1 = new User { Name = "Arthur", Role = "Admin" };
+                var user2 = new User { Name = "klim", Role = "User" };
+
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+            }
         }
     }
+    
 }
